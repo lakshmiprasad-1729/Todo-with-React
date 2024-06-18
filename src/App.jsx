f20384f35d3dfd41725d66b2cea9116ea7c80d0c
lakshmiprasad_1729 <panchagirilakshminarayana@gamil.com>
2024-06-18 20:28:02 +0530

    added some style to the todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 import './index.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     
     <div className='bg-gray-600 w-[100dvw] h-[100dvh]'>
     <AddTodo/>
+    <Todos/>
     </div>
   )
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+// import React from 'react'
 
 export default function Todos() {
   return (
-    <div>
-
-    </div>
+    <span className="flex mt-[4rem] lg:w-[64rem]
+      bg-black ml-[25dvw] ">
+      <div className="w-[4dvw] h-[3rem] bg-black  flex justify-center items-center ">
+        <button className="w-[1.2rem] h-[1.2rem] rounded-xl bg-neutral-300"></button>
+      </div>
+      <input type="text" placeholder='' 
+      className='w-[50dvw] h-[3rem]  outline-none bg-black text-white ' value={"hello world"} />
+      <button className="w-[4rem] h-[3rem] bg-red-500 rounded ml-[1rem]"></button>
+      <button className="w-[4rem] h-[3rem] bg-pink-500 rounded ml-[1rem] mr-[0.5rem]"></button>
+    </span>
   )
 }
