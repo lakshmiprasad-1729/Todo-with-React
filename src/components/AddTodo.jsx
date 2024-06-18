1e514917d3161586cb2651099b631b3d32438b9c
lakshmiprasad_1729 <panchagirilakshminarayana@gamil.com>
2024-06-18 21:41:09 +0530

    made some basic functionality

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,12 +1,30 @@
 import {useState} from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
+
 
 export default function AddTodo() {
+
+    const [input,setInput]=useState('');
+    const dispatch = useDispatch();
+
+    const handleClick=()=>{
+      dispatch(addTodo(input))
+      setInput('')
+    }
+
   return (
     <div>
         <div className='flex'>
           <input type="text" placeholder='enter todo task for ex:Complete writting assignment' 
-          className='w-[50dvw] h-[3rem]  ml-[25dvw] mt-[15dvh] rounded-xl outline-none'  />
-          <button className='bg-blue-500 w-[7rem] h-[3rem] mt-[15dvh] ml-[3rem] rounded-xl text-white'>AddTodo</button>
+          className='w-[50dvw] h-[3rem]  ml-[25dvw] mt-[15dvh] rounded-xl outline-none'
+          value={input} 
+          onChange={(e)=>setInput(e.target.value)}
+           />
+          <button
+          className='bg-blue-500 w-[7rem] h-[3rem] mt-[15dvh] ml-[3rem] rounded-xl text-white'
+          onClick={handleClick}
+          >AddTodo</button>
         </div>
     </div>
   )
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,16 +1,27 @@
-// import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 export default function Todos() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
   return (
-    <span className="flex mt-[4rem] lg:w-[64rem]
-      bg-black ml-[25dvw] ">
+    <>
+      <div className="ml-[49dvw] my-[1rem]">Todo</div>
+      {todos.map((todo) => (
+        // eslint-
+      <>
+      <div  id={todo.id} className="flex ml-[25dvw]">
       <div className="w-[4dvw] h-[3rem] bg-black  flex justify-center items-center ">
-        <button className="w-[1.2rem] h-[1.2rem] rounded-xl bg-neutral-300"></button>
+        <button className='w-[1.2rem] h-[1.2rem] rounded-xl'></button>
       </div>
       <input type="text" placeholder='' 
-      className='w-[50dvw] h-[3rem]  outline-none bg-black text-white ' value={"hello world"} />
-      <button className="w-[4rem] h-[3rem] bg-red-500 rounded ml-[1rem]"></button>
+      className='w-[50dvw] h-[3rem]  outline-none bg-black text-white ' value={todo.text}  disabled={true} />
+      <button className="w-[4rem] h-[3rem] bg-red-500 rounded ml-[1rem]" onClick={()=>dispatch(removeTodo(todo.id))}></button>
       <button className="w-[4rem] h-[3rem] bg-pink-500 rounded ml-[1rem] mr-[0.5rem]"></button>
-    </span>
-  )
+      </div>
+      </>
+      ))}
+    </>
+  );
 }
